412fb5bff4f66c136b412bb3ce26c9ee9ed3aa3f
Arief Maulana <ariefmavl@gmail.com>
2022-01-02 15:11:25 +0700

    Convert HTML to Svelte

---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title: 'First Charity Project',
+            category: 'Money',
+            thumbnail: 'https://live.staticflickr.com/65535/17102022497_bd3b59fa12_k.jpg',
+            pledged: 99000,
+            target: 100000,
+            date_end: +new Date('31 August 2022'),
+            profile_photo: 'https://live.staticflickr.com/7302/10560555684_ca020d7701_b.jpg',
+            profile_name: 'Arief Maulana',
+            no_pledges: 0
+        }
+    ]
+}
